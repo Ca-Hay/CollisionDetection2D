b86eb8dff165ccfef73e06a0627a03629c0663ec
cameron <me@you.com>
2022-03-04 13:13:20 +0000

    Code

---
+++ JS/Canvas.js
@@ -0,0 +1,35 @@
+const canvas = document.querySelector('canvas'
+)
+
+const c = canvas.getContext('2d')
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+const mouse = {
+    x: innerWidth / 2,
+    y: innerHeight /2
+}
+
+//Events
+addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX
+    mouse.y = event.clientY
+})
+
+//Animation Loop heheheha
+function animate() {
+    requestAnimationFrame(animate)
+    c.fillStyle = '#1A1A23'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    //redrect
+    c.fillStyle = '#E86262'
+    c.fillRect(mouse.x, mouse.y, 100, 100)
+
+    //bloorect
+    c.fillstyle = '#92ABEA'
+    c.fillRect(canvas.width / 2 - 50, canvas.height / 2 - 50, 100, 100)
+}
+
+animate()
+
