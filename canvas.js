51dfd3551dafb0bd0e0cadf5ba63f4ebd6e04476
cameron <me@you.com>
2022-03-05 14:55:33 +0000

    removed scrollbars

--- canvas.js
+++ canvas.js
@@ -1,5 +1,8 @@
-const canvas = document.querySelector('canvas'
+var canvas = document.querySelector('canvas'
 )
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 const c = canvas.getContext('2d')
 canvas.width = innerWidth
