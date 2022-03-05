f39d2e00726c9e79b8ea7cd2f06fdfd09fad8521
cameron <me@you.com>
2022-03-05 15:09:53 +0000

    among us

--- canvas.js
+++ canvas.js
@@ -18,30 +18,30 @@ addEventListener('mousemove', (event) => {
     mouse.x = event.clientX
     mouse.y = event.clientY
 })
-
 //Animation Loop 
 function animate() {
     requestAnimationFrame(animate)
     c.fillStyle = '#1A1A23'
     c.fillRect(0, 0, canvas.width, canvas.height)
 
+    
+    //need a condition in the animate loop
+    if(mouse.x + 100 >= canvas.width / 2 - 50 &&
+       mouse.x <= canvas.width / 2 - 50 + 100 &&
+       mouse.y + 100 >= canvas.height / 2 - 50 &&
+       mouse.y <= canvas.height / 2 - 50 + 100
+       ){
+           console.log("i am colliding")
+           
+    }
+
     //redrect
     c.fillStyle = '#E86262'
     c.fillRect(mouse.x, mouse.y, 100, 100)
 
     //bloorect
-    c.fillstyle = '#92ABEA'
+    c.fillStyle = '#92ABEA'
     c.fillRect(canvas.width / 2 - 50, canvas.height / 2 - 50, 100, 100)
-
-    const blueRectX = canvas.width / 2 - 50 + 100
-    const blueRectY = canvas.height / 2 - 50 + 100
-    //need a condition in the animate loop
-    if(mouse.x + 100 >= blueRectX  &&
-       mouse.x <= blueRectX        &&
-       mouse.y + 100 >= blueRectY  &&
-       mouse.y <= blueRectY){
-        console.log('colliding');
-    }
 }
 
 animate()
