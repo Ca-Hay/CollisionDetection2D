f9fa1f7ba89c02d815c05ef9aa9c6b801ae232f6
cameron <me@you.com>
2022-03-07 09:01:16 +0000

    among us

--- canvas.js
+++ canvas.js
@@ -11,7 +11,22 @@ const mouse = {
     x: innerWidth / 2,
     y: innerHeight / 2
 }
-
+function drawRed(){
+    //redrect
+    c.fillStyle = '#E86262'
+    c.fillRect(mouse.x, mouse.y, 100, 100)
+};
+function drawBlue(){
+    //bloorect
+    c.fillStyle = '#92ABEA'
+    c.fillRect(canvas.width / 2 - 50, canvas.height / 2 - 50, 100, 100)
+};
+function drawBothGreen(){
+    c.fillStyle = '#00ee00'
+    c.fillRect(mouse.x, mouse.y, 100, 100)
+    c.fillStyle = '#00ee00'
+    c.fillRect(canvas.width / 2 - 50, canvas.height / 2 - 50, 100, 100)
+}
 //Events
 addEventListener('mousemove', (event) => {
     mouse.x = event.clientX
@@ -31,17 +46,13 @@ function animate() {
        mouse.y <= canvas.height / 2 - 50 + 100
        ){
         collision = 1;
+        drawBothGreen();
     } else {
         collision = 0;
+        drawRed();
+        drawBlue();
     }
-
-    //redrect
-    c.fillStyle = '#E86262'
-    c.fillRect(mouse.x, mouse.y, 100, 100)
-
-    //bloorect
-    c.fillStyle = '#92ABEA'
-    c.fillRect(canvas.width / 2 - 50, canvas.height / 2 - 50, 100, 100)
+    
 }
 
 animate()
